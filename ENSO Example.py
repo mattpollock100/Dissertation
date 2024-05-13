9304f7376bee2ca83a1f729feabb93127b9f3a41
mattpollock100 <160645857+mattpollock100@users.noreply.github.com>
2024-05-13 15:09:54 +0100

    Add files via upload

--- ENSO Example.py
+++ ENSO Example.py
@@ -29,6 +29,9 @@ path = 'C:/Users/mattp/OneDrive/Desktop/Climate Change MSc/Dissertation/Data/Net
 sub_path = '/MPI_ESM/'
 file ='sst_Amon_MPI_ESM_TRSF_slo0043_100101_885012.nc'
 
+sub_path = '/IPSL_CM6/'
+file = 'TR6AV-Sr02_20000101_79991231_1M_t2m.nc'
+
 dataset = xarray.open_dataset(path + sub_path + file)
 
 chunk_years= 500
@@ -66,11 +69,9 @@ def find_enso_events(data, threshold, months_threshold):
     # Count the number of regions that have a size of 6 or more
     num_large_regions = np.count_nonzero(region_sizes >= months_threshold)
 
-    # Initialize lists to store average anomalies and lengths
     avg_anomalies = []
     lengths = []
 
-    # Loop over each region
     for i in range(1, num_features + 1):
         # Get the size of the region
         size = np.count_nonzero(labeled_array == i)
@@ -84,31 +85,36 @@ def find_enso_events(data, threshold, months_threshold):
             avg_anomaly = data[labeled_array == i].mean()
             avg_anomalies.append(avg_anomaly)
 
-    return [num_large_regions, avg_anomalies.mean(), lengths.mean()]
-    #print(f"The anomaly stays above 0.5 for 6 periods or more {num_large_regions} times.")
-
+    return [num_large_regions, round(np.mean(avg_anomalies),3), round(np.mean(lengths),1)]
+    
+    
 #%%
 
-#PROBABLY STILL NEED TO WEIGHT IT
+#STILL NEED TO WEIGHT IT??
 
-# Select the relevant enso regions
-data_hist = dataset['sst']
+#NEED TO CHANGE THE VARIABLE NAME FOR EACH DATA SET
+data_hist = dataset['tas']
 
 #Loop through time in chunks 
 max_time = data_hist.time.shape[0]
 chunk_size = chunk_years * 12
-output = []
+output_34 = []
+output_12 = []
+
 for i in range(0,max_time,chunk_size):
     print(f"Slicing {i}")
     data_slice = data_hist[i:i+chunk_size,:,:]
-    #data_slice = data_hist[0:6000,:,:]
-
+    
     #NEED TO DOUBLE CHECK THE ACTUAL DEFINITION OF THE ENSO REGIONS
-    #nino_34 = data_slice.sel(lat=slice(-5, 5), lon=slice(190, 240))  
-    nino_34 = data_slice.sel(lat=slice(5, -5), lon=slice(190, 240))  
-    nino_12 = data_slice.sel(lat=slice(0, -10), lon=slice(270, 280))
+    
+    ####NEED TO KNOW HOW LONGITUDINAL VALUES ARE DEFINED FOR EACH DATASET
+    #nino_34 = data_slice.sel(lat=slice(5, -5), lon=slice(190, 240))  
+    #nino_12 = data_slice.sel(lat=slice(0, -10), lon=slice(270, 280))
 
-    # Define the size of the rolling window, e.g., 5 years (12 months * 5)
+    nino_34 = data_slice.sel(lat=slice(5, -5), lon=slice(-170, -40))  
+    nino_12 = data_slice.sel(lat=slice(0, -10), lon=slice(-90, -80))
+    
+    # Define the size of the rolling window
     window_size = climatology_years * 12
 
     # Calculate the rolling climatology (mean for each calendar month over a running window)
@@ -119,6 +125,7 @@ for i in range(0,max_time,chunk_size):
     # Calculate anomalies by subtracting the rolling climatology from the full dataset
     anomalies_34 = nino_34 - climatology_34
     anomalies_12 = nino_12 - climatology_12
+
 
     # Plot the time series of the area-averaged anomalies
     anomalies_mean_34 = anomalies_34.mean(['lat', 'lon'])
@@ -136,7 +143,52 @@ for i in range(0,max_time,chunk_size):
     count_34 = find_enso_events(anomalies_mean_34.dropna('time'), threshold_34, months_threshold_34)
     count_12 = find_enso_events(anomalies_mean_12.dropna('time'), threshold_12, months_threshold_12)
 
-    output.append((int(i/12),count_34,count_12))
+    output_34.append((int(i/12),count_34))
+    output_12.append((int(i/12),count_12))
+
+#%%
+# Create a figure and a set of subplots
+def plot_anomaly_stats(output, title):
+    fig, ax1 = plt.subplots()
+
+    # Plot the first sub-element on the first y-axis
+    color = 'tab:blue'
+    ax1.set_xlabel('Model Year')
+    ax1.set_ylabel('Count', color=color)
+    ax1.plot([x[0] for x in output], [x[1][0] for x in output], color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    # Create a second y-axis for the second sub-element
+    ax2 = ax1.twinx()
+    color = 'tab:red'
+    ax2.set_ylabel('Mean Anomaly', color=color)
+    ax2.plot([x[0] for x in output], [x[1][1] for x in output], color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    # Create a third y-axis for the third sub-element
+    ax3 = ax1.twinx()
+    color = 'tab:green'
+    ax3.spines['right'].set_position(('outward', 60))
+    ax3.set_ylabel('Mean Length', color=color)
+    ax3.plot([x[0] for x in output], [x[1][2] for x in output], color=color)
+    ax3.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    plt.title(title)
+    plt.show()
+
+# %%
+
+plot_anomaly_stats(output_34, 'ENSO 3.4 Anomaly Stats')
+plot_anomaly_stats(output_12, 'ENSO 1+2 Anomaly Stats')
 
 
+# %%
+#THIS LOOKS LIKE IT WORKS TO SUBTRACT THE SEASONAL CLIMATOLGY
+data = nino_34
+window_size = 50 * 12
+climatology = data.groupby('time.month').apply(
+        lambda x: x.rolling(time=window_size, center=True, min_periods=1).mean())
 
+anomalies = data - climatology
+# %%
